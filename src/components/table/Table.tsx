4e88a43acfaf892e4b97eea01c9c6130076da766
msemera-dev <msemera.dev@gmail.com>
2024-01-05 21:56:16 +0800

    Initial commit for tsx and mui

---
+++ src/components/table/Table.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import { FunctionComponent } from "react";
+
+
+const Table: FunctionComponent = () => {
+  return (
+    <Box>Table</Box>
+  );
+}
+
+export default Table;
